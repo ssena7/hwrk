46914d3a9b7f8c0ba5ed5dac727fb4e728daf773
Sena Sümer <ssena6050@gmail.com>
2022-03-21 00:46:32 +0300

    i continued write my code

--- homework11/homework11/homework11.cpp
+++ homework11/homework11/homework11.cpp
@@ -2,3 +2,24 @@
 using  namespace std;
 int array[9];
 
+void rotation(int array[], int arraysize)
+{
+	int i;
+	int last = array[arraysize - 1];
+	for (i = arraysize - 2; i <= 0; i--);
+	{array[i - 1] = array[i]; }
+	array[0] = last;
+}
+
+
+int main() {
+
+
+
+	string N;
+	string direction;
+	int array[] = { 1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < 9; i++)
+	{
+		cout << array[i] << " ";
+	}
